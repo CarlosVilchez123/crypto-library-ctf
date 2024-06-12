bb6b06e542ff497017e614a061baef1fa9947512
CarlosVilchez123 <carloslvt123@gmail.com>
2024-06-11 20:12:39 -0500

    se corrigio una falta ortografica :c

---
+++ Theorems/chinise_remainder_theorem.py
@@ -0,0 +1,16 @@
+from factrizaciones.GCD_extends import extended_gcd
+
+def CRT(N_list_modules, list_rest):
+    sum = 0
+    prod = 1
+    for ni in N_list_modules:
+        prod *=ni
+    
+    for ni, ai in zip(N_list_modules, list_rest):
+        p = prod // ni
+        g,x,y = extended_gcd(p,ni)
+        if g != 1:
+            raise ValueError(b'los modulos no son coprimos')
+        sum += ai*x*p
+    
+    return sum % prod
